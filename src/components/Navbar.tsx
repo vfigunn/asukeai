6f98ccbe5d0352f44851b7c1441c7ecab4e7f735
vfigunn <vfigun@gmail.com>
2025-05-21 12:05:35 -0300

    Mejoras generales

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import { Link, useLocation } from 'react-router-dom';
 import { Menu, X } from 'lucide-react';
 
 const Navbar = () => {
+  const whatsappMessage = 'https://api.whatsapp.com/send?phone=+595993367902&text=Hola,%20te%20quiero%20publicar%20un%20evento'
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const location = useLocation();
   
@@ -11,15 +12,18 @@ const Navbar = () => {
 
   return (
     <nav className="shadow-sm sticky top-0 z-50 bg-notionbody">
-      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+      <div className="container mx-auto px-4 py-2 flex justify-between items-center">
         <Link to="/" className="flex items-center gap-2">
-          <span className="font-norwester font-bold text-2xl text-white">Asu<span className="text-red-500">keai</span></span>
+          <span className="font-norwester font-bold text-2xl text-white"><a href="/"><img src="/assets/header_asukeai.gif" alt="header_asukeai" className='h-20 rounded-lg' /></a></span>
         </Link>
         
         {/* Desktop menu */}
         <div className="hidden md:flex items-center gap-8">
           <Link to="/" className={`font-norwester font-medium transition-colors ${isActive('/') ? 'text-primary' : 'text-white hover:text-primary'}`}>
             Eventos
+          </Link>
+          <Link to={whatsappMessage} target="_blank" className={`font-norwester font-medium transition-colors ${isActive('') ? 'text-white' : 'text-red-500 hover:text-white'}`}>
+            Publicar Evento
           </Link>
           <Link to="/nosotros" className={`font-norwester font-medium transition-colors ${isActive('/nosotros') ? 'text-primary' : 'text-white hover:text-primary'}`}>
             Nosotros
